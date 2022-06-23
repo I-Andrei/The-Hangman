50cfcbb242aa822d5c0fcb389dd1648d0457436e
I-Andrei <78320950+I-Andrei@users.noreply.github.com>
2022-06-23 14:47:41 +0300

    add files

---
+++ index1.js
@@ -0,0 +1,12 @@
+var newWord = "", var wordReady = "";
+
+function inputNewWord(){
+  newWord = document.getElementById("newWord").value
+  workReady = newWord
+  workReady[1]='_'
+   document.getElementById('MyID').innerHTML = workReady
+ }
+
+ function inputLetter(){
+  var z = document.getElementById("newLetter").value
+ }
